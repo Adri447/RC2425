99a9a5efc6206fdd63222175d84c598724899afb
Adri447 <adrian.cumbreras@alu.uhu.es>
2025-02-27 09:52:12 +0100

    Update wedding_woes.pl

--- exercism/wedding_woes.pl
+++ exercism/wedding_woes.pl
@@ -1,1 +1,15 @@
 
+chatty(gustavo).
+chatty(valeria).
+
+likes(esteban, malena).
+likes(malena, esteban).
+likes(gustavo, valeria).
+
+pairing(A, _):- chatty(A).
+pairing(_, A):- chatty(A).
+
+% La coma es como una Y
+pairing(A, B):- likes(A, B), likes(B, A).
+
+seating(A, B, C, D, E):- pairing(A, B), pairing(B, C), pairing(C, D), pairing(D, E), pairing(E, A)
