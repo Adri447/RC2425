f0303823420c3b4e630ba41fa7bff3e49b2f49f3
Adri447 <adrian.cumbreras@alu.uhu.es>
2025-02-27 09:54:44 +0100

    Update wedding_woes.pl

--- exercism/wedding_woes.pl
+++ exercism/wedding_woes.pl
@@ -12,4 +12,4 @@ pairing(_, A):- chatty(A).
 % La coma es como una Y
 pairing(A, B):- likes(A, B), likes(B, A).
 
-seating(A, B, C, D, E):- pairing(A, B), pairing(B, C), pairing(C, D), pairing(D, E), pairing(E, A)
+seating(A, B, C, D, E):- pairing(A, B), pairing(B, C), pairing(C, D), pairing(D, E), pairing(E, A).
