6ab924c86ae6e77b54e22647fb7ee8203ed73f1f
Adri447 <adrian.cumbreras@alu.uhu.es>
2025-03-20 09:31:55 +0100

    Update notas.pl

--- notas.pl
+++ notas.pl
@@ -34,55 +34,6 @@ natural(N):- N > 1, N2 is N-1, natural(N2).
 
 
 /*
-Listas:
-n0 = [], [_] < [_,_]
-
-[Cabeza|Resto] = [1,2,3,4].
-Cabeza tiene el primero
-*/
-
-/*
-my_length(list, Num)
- es cierto si Num unifica con el numero de elementos de List.
-*/
-
-my_Length([], 0).
-my_Length([_|Resto], N):- R is N-1, my_Length(Resto, R).
-
-/*
-elemento_enesimo(+Lista, +Pos, -Elem)
-  es cierto cuando Elem unifica con el elemento de la posición Pos en de Lista.
-*/
-
-elemento_enesimo([Cab|_], 1, Cab).
-elemento_enesimo([_|Resto], N, R):- N > 1, N2 is N-1, elemento_enesimo(Resto, N2, R).
-
-/*
-my_reverse(?List, ?ListR) 
-  es cierto cuando ListR unifica con una lista que contiene los mismos elementos que List pero en orden inverso.
-
-append([1,2,3],[4,5],R).
-R = [1,2,3,4,5]
-*/
-
-my_reverse([],[]).
-my_reverse([Cab|Resto], R2):- my_reverse(Resto, R), append(R, [Cab], R2).
-
-
-/*
-comprime(Lista, R)
- es cierto si R unifica con la lista si:
- comprime([a,a,a,b,b,c,a,a], R).
- R = [(a,3),(b,2),(c,1),(a,2)]
-*/
-
-
-
-
-
-
-
-/*
 Arbol
 nil,
 
