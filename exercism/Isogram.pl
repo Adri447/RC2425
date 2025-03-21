a31737d35ea59df97f690b829140e968c17e5c3e
Adri447 <adrian.cumbreras@alu.uhu.es>
2025-03-21 13:49:59 +0100

    Update Isogram.pl

--- exercism/Isogram.pl
+++ exercism/Isogram.pl
@@ -1,1 +1,25 @@
 
+/*
+isogram(String).
+  es cierto si la cadena String contiene caracteres diferentes
+  excepto los espacios en blanco y los guiones.
+
+string_lower(+String, string).
+string_codes(?String, ?Codes).
+
+quita_blancos(Lista, R).
+  es cierto si R unifica con una lista de números que contiene Lista
+  eliminando las ocurrencias de 32 y 45 (" " y "-").
+  
+*/
+
+blanco(32).
+blanco(45).
+
+
+isogram(String):- maplist(all_distinct(R)), exclude(blanco, Codes, R).
+
+string_lower(String, string).
+string_codes(string, Codes).
+
+
