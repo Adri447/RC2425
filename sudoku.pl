8831b3246bc8bb7c528df939427ee159a69daf86
Adri447 <adrian.cumbreras@alu.uhu.es>
2025-03-21 11:22:48 +0100

    Update sudoku.pl

--- sudoku.pl
+++ sudoku.pl
@@ -1,14 +1,17 @@
 /*
 
 sudoku(Matrix).
-  es cierto si Matrix unifica con una matriz 9x9 casillas, dividido en 9 bloques de 3x3 casillas,
-  con los numeros del 1 al 9, con numeros distintos or fila, columnas y por cada bloque 3x3
-
+  es cierto si Matrix unifica con una matriz 9x9 casillas,  /*16*/
+  con los números del 1 al 9,  /*17*/
+  dividido en 9 bloques de 3x3 casillas, /*21*/
+  con números distintos or fila, /*18*/
+  columnas /*20*/
+  y por cada bloque 3x3
 
 */
 
 
-sudoku(Rows) :-
+sudoku(Rows):-
         length(Rows, 9),
         maplist(same_length(Rows), Rows),
         append(Rows, Vs), Vs ins 1..9,
@@ -21,6 +24,6 @@ sudoku(Rows) :-
         blocks(Gs, Hs, Is).
 
 blocks([], [], []).
-blocks([N1,N2,N3|Ns1], [N4,N5,N6|Ns2], [N7,N8,N9|Ns3]) :-
+blocks([N1,N2,N3|Ns1], [N4,N5,N6|Ns2], [N7,N8,N9|Ns3]):-
         all_distinct([N1,N2,N3,N4,N5,N6,N7,N8,N9]),
         blocks(Ns1, Ns2, Ns3).
