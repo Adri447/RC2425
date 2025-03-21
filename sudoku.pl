0dade5a9b375906c3e5a184c130f80e21f0ecdb5
Adri447 <adrian.cumbreras@alu.uhu.es>
2025-03-21 10:39:12 +0100

    Create sudoku.pl

---
+++ sudoku.pl
@@ -0,0 +1,8 @@
+/*
+
+sudoku(Matrix).
+  es cierto si Matrix unifica con una matriz 9x9 casillas, dividido en 9 bloques de 3x3 casillas,
+  con los numeros del 1 al 9, con numeros distintos or fila, columnas y por cada bloque 3x3
+
+
+*/
