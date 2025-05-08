3f9446bb412f769d64a0999d7dcd1ae92d665bad
Adri447 <adrian.cumbreras@alu.uhu.es>
2025-05-08 09:39:59 +0200

    Update Arboles.pl

--- Arboles.pl
+++ Arboles.pl
@@ -16,6 +16,13 @@ Induccion:
 
 */
 
+arbol1(
+        a(1, a(2,nil,nil), a(3,nil,nil))
+        ).
+        
+arbol2(
+        a(1, a(2, a(4, nil, nil), a(5, nil, nil)), a(3, a(6, nil, nil), a(7, a(8, nil, nil), nil)))
+        ).
 
 /* 
 cuenta_nodos(+Arbol_binario, ?Num_nodos)
@@ -25,3 +32,50 @@ numero de nodos del árbol "Arbol_binario"
 
 cuenta_nodos(nil, 0).
 cuenta_nodos(a(_, AI, AD), R):- cuenta_nodos(AI, RI), cuenta_nodos(AD, RD), R is RI + RD + 1.
+
+/*
+crea_arbol(Lista. Arbol_binario)
+es cierto cuando Arbol_binario unifica con un arbol binerio balanceado
+que contiene todas las etiquetas de la Lista.
+
+*/
+
+/* length  y append son reversibles */
+
+crea_arbol([], nil).
+crea_arbol([Cab|Resto], a(Cab, A1, A2)):- length(Resto, L), Med is L div 2, length(L1, Med), append(L1, L2, Resto),
+        crea_arbol(L1, A1), crea_arbol(L2, A2).
+
+
+/*
+balanceado(Arbol)
+es cierto si "para todo nodo del arbol" la diferencia entre la altura
+del subarbol Izq y el subarbol Der sea como maximo 1.
+
+*/
+
+
+
+
+
+/*
+altura(Arbol, A)
+es cierto si A unifica con la altura de un arbol_binario.
+
+*/
+
+altura(nil, 0).
+altura(a(_, AI, AD), A):- altura(AI, Ai), altura(AD, Ad), Ai > Ad, A is Ai + 1.
+altura(a(_, AI, AD), A):- altura(AI, Ai), altura(AD, Ad), Ai =< Ad, A is Ad + 1.
+
+
+
+
+
+
+
+
+
+
+
+
