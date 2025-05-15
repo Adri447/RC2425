e5ff3c0a0b757e517144b182b30dca0d53f029ba
Adri447 <adrian.cumbreras@alu.uhu.es>
2025-05-15 09:59:15 +0200

    Update Grafos.pl

--- Grafos.pl
+++ Grafos.pl
@@ -12,7 +12,9 @@ c _ d
 
 */
 
-g([a,b,c,d], [a(a,b),a(c,d),a(a,c),a(c,d)]).
+grafo1(
+	g([a,b,c,d], [a(a,b),a(b,c),a(a,c),a(c,d)])
+	).
 
 /*
 Ini ---- Arista ---- TMP --- Camino ---- FIN
@@ -41,20 +43,19 @@ Induccion:
 
 /*
 
-1) camino: No, Dirigido, Lista de Vertices, Visitados = Vertices, N0 -> Ini = Fin
+1) camino: No Dirigido, Lista de Vertices, Visitados = Vertices, N0 -> Ini = Fin
 camino(g(Vertices, Aristas), Ini, Fin, Visitados, Camino)
   es cierto si camino unifica con ua lista de vertices para llegar 
   desde Ini hasta Fin sin repetir vertices visitados en el grafo g
 
 */
 
-1)
 camino(_, Ini, Ini, _, [Ini]).
 camino(g(Vertices, Aristas), Ini, Fin, Visitados, [Ini|Camino]):-
   conectado(g(Vertices, Aristas), Ini, TMP),
-  camino(g(Vertices, Aristas), TMP, Fin, [TMP|Visitados], Camino),
+  \+ member(TMP, Visitados),
+  camino(g(Vertices, Aristas), TMP, Fin, [TMP|Visitados], Camino).
   
-
 
 /*
 conectado(Grafo, A, B)
@@ -69,4 +70,40 @@ conectado(g(_, Aristas), Ini, Fin):- member(a(Fin,Ini), Aristas).
 conectado(g(_, Aristas), Ini, Fin):- member(a(Ini,Fin), Aristas).
 conectado(g(_, Aristas), Ini, Fin):- member(a(Fin,Ini), Aristas).
 
+
+grafo2(
+	g([a,b,c,d,e], [a(a,b),a(a,c),a(b,c),a(c,e),a(c,d),a(b,d),a(d,e),a(b,e)])
+	).
+
+/*
+Ejercicio de la casa sin pasar por la arista mas de un vez
+2) camino: No Dirigido, Lista de Aristas, Visitados = Ariatas, N0 -> Existe Arista entre Ini y Fin
+camino(g(Vertices, Aristas), Ini, Fin, Visitados, Camino)
+  es cierto si camino unifica con ua lista de Aristas para llegar 
+  desde Ini hasta Fin sin repetir Aristas visitadas en el grafo g
+
+*/
+
+camino(G, Ini, Fin, _, []):- conectado(G, Ini, Fin).
+camino(g(Vertices, Aristas), Ini, Fin, Visitados, [a(Ini,TMP)|Camino]):-
+  conectado(g(Vertices, Aristas), Ini, TMP),
+  \+ member(a(Ini,TMP), Visitados),
+  camino(g(Vertices, Aristas), TMP, Fin, [a(Ini,TMP),a(TMP,Ini)|Visitados], Camino).
+
+
+
+/*
+Obtener todos los ciclos de un grafo
+*/
+
+camino(G, Ini, Fin, Visitados, [a(Ini,Fin)]):- \+ member(a(Ini,Fin), Visitados), conectado(G, Ini, Fin).
+camino(g(Vertices, Aristas), Ini, Fin, Visitados, [a(Ini,TMP)|Camino]):-
+  conectado(g(Vertices, Aristas), Ini, TMP),
+  \+ member(a(Ini,TMP), Visitados),
+  camino(g(Vertices, Aristas), TMP, Fin, [a(Ini,TMP),a(TMP,Ini)|Visitados], Camino).
+
+
+
+
+  
 
