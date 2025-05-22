36e170ca2f8b93cc2662389e7d0cbc6614e9f390
Adri447 <adrian.cumbreras@alu.uhu.es>
2025-05-22 09:41:11 +0200

    Update Horario.pl

--- Horario.pl
+++ Horario.pl
@@ -1,3 +1,21 @@
+
+slots_per_week(35).
+
+slots_per_day(7).
+
+class_subject_teacher_times('1a', deu, sjk1, 4).
+class_subject_teacher_times('1a', mat, mat1, 5).
+class_subject_teacher_times('1a', eng, anj1, 3).
+class_subject_teacher_times('1a', h, zgo1, 2).
+class_subject_teacher_times('1a', geo, geo1, 2).
+class_subject_teacher_times('1a', ch, kem1, 2).
+class_subject_teacher_times('1a', bio, bio1, 2).
+class_subject_teacher_times('1a', ph, fiz1, 2).
+class_subject_teacher_times('1a', f, rai1, 2).
+class_subject_teacher_times('1a', lat, atvz1, 3).
+
+
+
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    Simsttab -- Simplistic school time tabler
    Copyright (C) 2005-2022 Markus Triska triska@metalevel.at
